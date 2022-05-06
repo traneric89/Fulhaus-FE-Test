3ae38d84cb92cba3b798f75e405b82054939c742
optiX <55358375+traneric89@users.noreply.github.com>
2022-05-06 19:51:42 -0400

    product api call

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { useState } from "react";
-import itemImg from "../images/chair.avif";
 
-const Product = () => {
+const Product = ({ name, brand, price, imgURL }) => {
   const [display, setDisplay] = useState({ display: "none" });
   const [style, setStyle] = useState({ backgroundColor: "" });
 
@@ -19,15 +18,15 @@ const Product = () => {
       }}
       style={style}
     >
-      <img src={itemImg} alt="" className="item-img" />
+      <img src={imgURL} alt="" className="item-img" />
       <div className="img-text" style={display}>
         <div className="product-info">
-          <h2>Product Name</h2>
-          <h4>BRAND NAME</h4>
+          <h2>{name}</h2>
+          <h4>{brand}</h4>
         </div>
         <div className="product-cart">
           <h6 style={{ cursor: "pointer" }}>+ Add to Cart</h6>
-          <h4>$900</h4>
+          <h4>${price}</h4>
         </div>
       </div>
     </div>
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,15 +1,32 @@
 import React from "react";
 import Product from "./Product";
+import { useState, useEffect } from "react";
 
 const Products = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const res = await fetch(
+        "https://fh-api-dev.herokuapp.com/api/products-service/products/website/CAD?page=0&limit=6"
+      );
+      const { data } = await res.json();
+      setData(data.products);
+    };
+    fetchEvents();
+  }, []);
+
   return (
     <div className="products-grid">
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
+      {data.map((product) => (
+        <Product
+          key={product._id}
+          imgURL={product.imageURLs[0]}
+          name={product.fulhausProductName}
+          brand={product.material}
+          price={product.retailPrice}
+        />
+      ))}
     </div>
   );
 };
